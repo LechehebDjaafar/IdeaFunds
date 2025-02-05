09d8fb971851dc31ec170dd6be07d467a944e96e
djaafar <djaafardjaafarlolo@gmail.com>
2025-02-05 15:02:48 +0100

    IdeaFunds

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    content: [
+      './templates/**/*.html', // استهدف جميع ملفات HTML في مجلد templates
+    ],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  }
